f17b0b68b865a4294e2a41e61a4d18ca593914ed
Philip King <ilovethechiefs@gmail.com>
2016-09-22 19:21:42 -0500

    Add login route and authenicate class method

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
-module.exports = function (sequelize, DataTypes) {
-	return sequelize.define('user', {
+module.exports = function(sequelize, DataTypes) {
+	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
@@ -10,7 +10,7 @@ module.exports = function (sequelize, DataTypes) {
 			validate: {
 				isEmail: true
 			}
-		}, 
+		},
 		salt: {
 			type: DataTypes.STRING
 		},
@@ -21,9 +21,9 @@ module.exports = function (sequelize, DataTypes) {
 			type: DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
-				len: [7,100]
+				len: [7, 100]
 			},
-			set: function (value) {
+			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
 				var hashedPassword = bcrypt.hashSync(value, salt);
 
@@ -34,17 +34,41 @@ module.exports = function (sequelize, DataTypes) {
 		}
 	}, {
 		hooks: {
-			beforeValidate: function (user, options) {
+			beforeValidate: function(user, options) {
 				if (typeof(user.email) === 'string') {
 					user.email = user.email.toLowerCase();
 				}
 			}
 		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof(body.email) !== 'string' || typeof(body.password) !== 'string') {
+						return reject();
+					}
+
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+						resolve(user);
+					}, function(e) {
+						return reject();
+					});
+				});
+			}
+		},
 		instanceMethods: {
-			toPublicJSON: function () {
+			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
 			}
 		}
 	});
+
+	return user;
 };
